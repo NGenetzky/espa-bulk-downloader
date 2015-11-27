66835f77ba70d4da2f3c9c6a2433ced6607d5c67
NGenetzky <NGenetzky@usgs.gov>
2015-11-27 16:53:37 -0600

    Added setup file. Will be installed by setup.py install but package is empty

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="espa_bulk_downloader",
+
+    # Version number:
+    version="1.0",
+
+    # Application author details:
+    author="David V. Hill",
+    author_email="@usgs.gov",
+
+    license=open('LICENSE.txt').read(),
+    long_description=open('README.md').read(),
+
+    # Packages
+    packages=["espa_bulk_downloader"],
+
+    # Scripts
+    scripts=["espa_bulk_downloader/download_espa_order.py"],
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "feedparser <= 5.1.2, >=5.1.2"
+    ],
+)
