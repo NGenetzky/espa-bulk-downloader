f8cb6eba5f6a3c393ca07b44b9054c0c93287945
NGenetzky <NGenetzky@usgs.gov>
2015-12-03 11:58:22 -0600

    Added classifiers.

--- setup.py
+++ setup.py
@@ -4,28 +4,34 @@ from setuptools import setup
 
 setup(
     # Application name:
-    name="espa_bulk_downloader",
+    name='espa_bulk_downloader',
 
     # Version number:
-    version="1.0",
+    version='1.0.0',
 
     # Application author details:
-    author="David V. Hill",
-    author_email="@usgs.gov",
+    author='David V. Hill',
 
     license=open('LICENSE.txt').read(),
+    description='Client for downloading ESPA scenes.',
     long_description=open('README.md').read(),
 
+    classifiers = [
+        'Programming Language :: Python',
+        'Programming Langauge :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: GIS'
+    ],
+
     # Packages
-    packages=["espa_bulk_downloader"],
+    packages=['espa_bulk_downloader'],
 
     # Scripts
     # Moves the script to the user's bin directory so that it can be executed.
     # Usage is 'download_espa_order.py' not 'python download_espa_order.py'
-    scripts=["espa_bulk_downloader/download_espa_order.py"],
+    scripts=['espa_bulk_downloader/download_espa_order.py'],
 
     # Dependent packages (distributions)
     install_requires=[
-        "feedparser <= 5.1.2, >=5.1.2"
+        'feedparser <= 5.1.2, >=5.1.2'
     ],
 )
